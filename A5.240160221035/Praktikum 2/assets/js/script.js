da1bd29adf8b6a3c88624bfca7fd9c288d726d6b
RahmaNadya240160221035-source <240160221035@student.unsap.ac.id>
2026-05-10 20:35:40 +0700

    Selesai mengerjakan konsep dasar modul JavaScript

---
+++ A5.240160221035/Praktikum 2/assets/js/script.js
@@ -0,0 +1,21 @@
+const title = 'Belajar JavaScript';
+alert(title); 
+console.log('Selamat datang');
+
+const umur = 12;
+
+if (umur < 12) {
+  console.log('Kamu masih kecil');
+} else {
+  console.log('Kamu sudah besar');
+}
+
+setTimeout(() => {
+  console.log('Timer finished');
+}, 1000 );
+
+if (true) {
+  let hobi = 'Mendengarkan Musik';
+  console.log(hobi); 
+}
+
